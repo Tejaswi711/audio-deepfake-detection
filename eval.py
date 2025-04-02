2bdd3cbce26c4e1e5a1caf1df750f9307efa641e
Tejaswi711 <20x31a6651@gmail.com>
2025-04-03 00:11:05 +0530

    Delete eval.py

--- eval.py
+++
@@ -1,8 +0,0 @@
-import torch
-from torch.utils.data import DataLoader
-from dataset import ASVSpoofDataset
-from model import RawNet2
-import argparse
-from sklearn.metrics import classification_report, confusion_matrix
-import matplotlib.pyplot as plt
-import seaborn as sns
